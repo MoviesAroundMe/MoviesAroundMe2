cc476c4ae38ad489a9298f4e3f1be2a4fe84279f
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-20 15:37:35 +0100

    updates

--- public/app.js
+++ public/app.js
@@ -2,8 +2,11 @@ var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 
 moviesAroundMe.controller('moviesControl', ['Search', 'CineWorld', function(Search, CineWorld) {
   var self = this;
+  
+  console.log('Sammy :-)');
+  CineWorld.makeRequest().then(function(data) {
+    // console.log("after the then");
+    // console.log(data);
+  });
 
-  console.log('Helllloooooooooooooooo!');
-  console.log(Search);
-  console.log(CineWorld);
 }]);
--- public/services/cineWorld.js
+++ public/services/cineWorld.js
@@ -1,5 +1,16 @@
 'use strict';
 
-moviesAroundMe.factory('CineWorld', function() {
-  return 'CineWorld Factory return value'
-});
+moviesAroundMe.factory('CineWorld', ['$http', function($http) {
+    return {
+      makeRequest: function() {
+        console.log("inside the factory");
+        return $http({        
+        method: 'JSONP',
+        url: 'http://www.cineworld.co.uk/api/quickbook/cinemas?key=bHmPnV2t&full=true&callback=JSON_CALLBACK'
+      }).then(function(data){
+        console.log(data);
+      });
+      return 1
+    }
+  };
+}]);
