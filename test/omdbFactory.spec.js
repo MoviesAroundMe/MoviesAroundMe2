fa8b3c727011b289c9acd338780daa0015cf2935
Tomiblanchard <tomiblanchard@gmail.com>
2015-05-20 19:06:59 +0100

    tested factory but karma parse error

---
+++ test/omdbFactory.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('omdb api service', function(){
+  beforeEach(module('OMDb'));
+
+  var omdbFactory, httpBackend;
+
+  beforeEach(inject(function(_omdbFactory_, $httpBackend){
+    omdbFactory = _omdbFactory_;
+    httpBackend = $httpBackend;
+  }));
+
+  it("should do something", function() {
+    httpBackend.whenGET('http://www.omdbapi.com/?t=big&y=&plot=short&r=json&tomatoes=true').respond({
+      Title: "Big"
+    });
+    omdbFactory.makeRequest("big").then(function(Title){
+      expect(Title).toEqual(["Big"]);
+    });
+    httpBackend.flush();
+  });
+});
