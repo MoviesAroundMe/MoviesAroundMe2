8ccecd08343f4ee6e2f4a9a7e80b16ac509cfbb2
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-05-20 16:30:03 +0100

    adds array of cinemas with id, name & postcode

--- public/services/cineWorld.js
+++ public/services/cineWorld.js
@@ -1,8 +1,16 @@
 'use strict';
+var cinemas = [];
 
     function angularcallbacks_0(data) {
       console.log(data.cinemas[0].name);
-      console.log(data.cinemas[1].name);
+      console.log(data.cinemas.length);
+      for (var i = 0; i < data.cinemas.length; i++) {
+  		cinemas.push([]);
+  		cinemas[i].push(data.cinemas[i].id);
+  		cinemas[i].push(data.cinemas[i].name);
+  		cinemas[i].push(data.cinemas[i].postcode);
+  	  }
+  	  console.log(cinemas);
     }
 
 moviesAroundMe.factory('CineWorld', ['$http', function($http) {
