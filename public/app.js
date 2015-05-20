6fcd21c35dffb8b07c1c58edfba698c3f4a608f5
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-20 16:03:38 +0100

    updates the cine world service to deal with JSONP

--- public/app.js
+++ public/app.js
@@ -3,10 +3,7 @@ var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 moviesAroundMe.controller('moviesControl', ['Search', 'CineWorld', function(Search, CineWorld) {
   var self = this;
   
-  console.log('Sammy :-)');
-  CineWorld.makeRequest().then(function(data) {
-    // console.log("after the then");
-    // console.log(data);
-  });
+  console.log('In the controller');
+  CineWorld.makeRequest();
 
 }]);
--- public/services/cineWorld.js
+++ public/services/cineWorld.js
@@ -1,16 +1,15 @@
 'use strict';
 
+    function angularcallbacks_0(data) {
+      console.log(data.cinemas[0].name);
+      console.log(data.cinemas[1].name);
+    }
+
 moviesAroundMe.factory('CineWorld', ['$http', function($http) {
     return {
       makeRequest: function() {
-        console.log("inside the factory");
-        return $http({        
-        method: 'JSONP',
-        url: 'http://www.cineworld.co.uk/api/quickbook/cinemas?key=bHmPnV2t&full=true&callback=JSON_CALLBACK'
-      }).then(function(data){
-        console.log(data);
-      });
-      return 1
-    }
-  };
+        return $http.jsonp('http://www.cineworld.co.uk/api/quickbook/cinemas?key=bHmPnV2t&full=true&callback=JSON_CALLBACK');
+      }
+    };
 }]);
+    
