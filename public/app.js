ab405506b595fe449a6ac696891ccba1b2e7cd86
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-20 10:51:17 +0100

    setting cineWorld service

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 
-moviesAroundMe.controller('moviesControl', ['Search', function(Search) {
+moviesAroundMe.controller('moviesControl', ['Search', 'CineWorld', function(Search, CineWorld) {
   var self = this;
 
   console.log('Helllloooooooooooooooo!');
   console.log(Search);
+  console.log(CineWorld);
 }]);
---
+++ public/services/cineWorld.js
@@ -0,0 +1,5 @@
+'use strict';
+
+moviesAroundMe.factory('CineWorld', function() {
+  return 'CineWorld Factory return value'
+});
