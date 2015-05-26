7323d8a57edb1b3abd39baaaa5c041c39fe1020f
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-26 11:20:34 +0100

    last changes on Friday before the bank holiday

--- public/app.js
+++ public/app.js
@@ -1,26 +1,73 @@
 var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 
-moviesAroundMe.controller('moviesControl', ['Aggregator', function(Aggregator) {
+var angularcallbacks_0 = function (data) {
+  cineWorldCinemas = data.cinemas.map(function(cinema){
+    return [cinema.id, cinema.name, cinema.postcode];
+  });
+
+  cineWorldCinemas.forEach(function(i) {
+    this.cinemasArray.push(i[2]);
+  });
+};
+moviesAroundMe.controller('moviesControl', ['$resource', 'Aggregator', 'CineWorld', '$http', function($resource, Aggregator, CineWorld, $http) {
   var self = this;
-  self.array = [];
+  var cineWorldAPI;
    
-  this.updateArray = function() {
-    Aggregator.getName()
-      .then(
-      /* success function */
-      function(name) {
-        self.array.push(name.data);
-      }).then(function() {
-        console.log(self.array);
+  // this.updateArray = function() {
+  //   console.log("1");
+  //   CineWorld.makeRequest()
+  //     .then(
+  //     /* success function */
+  //     function(name) {
+  //       console.log("2");
+  //       self.array.push(name.data);
+  //     }).then(function() {
+  //       onsole.log("3");
+  //       console.log(self.array);
+  //     });
+  // };
+
+
+  // this.updateArray()
+
+  cineWorldAPI = $resource('http://www.cineworld.co.uk/api/quickbook/cinemas',
+    {callback: "JSON_CALLBACK"},
+    {key: 'bHmPnV2t'},
+    {full: true},
+    {get: {
+      method: "JSONP"
+      }
+    }
+   );
+        
+  // self.cineWorldCinemas = cineWorldAPI.get();
+
+  // .then(functon(data) {
+  //   console.log(data);
+  // });
+     // var url = "https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK";
+
+     var url = "http://www.cineworld.co.uk/api/quickbook/cinemas?key=bHmPnV2t&full=true&format=json&jsoncallback=JSON_CALLBACK";
+
+     $http.jsonp(url)
+      .then(function(data) {
+        console.log('data: ' + data)
       });
-  };
-
-  this.getCinemas = function() {
-
-  };
-
-  this.updateArray();
-
 
 }]);
 
+
+  // this.updateArray = function() {
+  //   Aggregator.getName()
+  //     .then(
+  //     /* success function */
+  //     function(name) {
+  //       self.array.push(name.data);
+  //     }).then(function() {
+  //       console.log(self.array);
+  //     });
+  // };
+
+  // this.getCinemas = function() {
+
+  // };
--- public/services/cineWorld.js
+++ public/services/cineWorld.js
@@ -1,19 +1,41 @@
 'use strict';
 
-function angularcallbacks_0(data) {
-  var cineWorldCinemas = data.cinemas.map(function(cinema){
-    return [cinema.id, cinema.name, cinema.postcode];
-  })
-};
+
 
 moviesAroundMe.factory('CineWorld', ['$http', function($http) {
-  
+  // var self = this;
+  // var cineWorldCinemas = [];
+  // var cinemas = [];
+
+  // function angularcallbacks_0(data) {
+  //   cineWorldCinemas = data.cinemas.map(function(cinema){
+  //     return [cinema.id, cinema.name, cinema.postcode];
+  //   });
+
+  //   cineWorldCinemas.forEach(function(i) {
+  //     cinemas.push(i[2]);
+  //   });
+  //   return cinemas;
+  // };
   return {
     makeRequest: function() {
-      return $http.jsonp('http://www.cineworld.co.uk/api/quickbook/cinemas?key=bHmPnV2t&full=true&callback=JSON_CALLBACK');
-    }
-  }
+      // $http.jsonp('http://www.cineworld.co.uk/api/quickbook/cinemas?key=bHmPnV2t&full=true&jsoncallback=JSON_CALLBACK')
+      //   .success(function(data) {
+      //     self.c
+          
+      //   })
 
+     var url = "https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK";
+
+     $http.jsonp(url)
+      .success(function(data) {
+        $scope.photos = (data.items);
+      });
+    },
+
+
+
+  }
 
 }]);
     
