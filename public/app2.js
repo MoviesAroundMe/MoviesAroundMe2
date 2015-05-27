497d8690a0d6938e619c3475662163b75fe36841
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-05-27 16:24:26 +0100

    postcode search links to google maps zebra

--- public/app2.js
+++ public/app2.js
@@ -3,13 +3,17 @@ var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 moviesAroundMe.controller('moviesControl', function() {
   self = this;
 
-  movie1 = {Title:"Big",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km"};
-  movie2 = {Title:"Home",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km"};
-  movie3 = {Title:"Forrest",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km"};
-  self.movies = [movie1, movie2, movie3];
+  movie1 = {Title:"Big",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km", Cinema: "W149AX"};
+  movie2 = {Title:"Home",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km", Cinema: "E162RU"};
+  movie3 = {Title:"Forrest",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km", Cinema: "NN33NY"};
 
-  updateMovies = function() {
-    //update self.movies to service call
+  self.updateMovies = function() {
+    console.log('hello');
+    self.movies = [movie1, movie2, movie3];
+  }
+
+  self.updateMap = function(cinema) {
+  	$("#map-frame").attr('src', 'https://www.google.com/maps/embed/v1/directions?origin='+self.postcode+'&destination='+cinema+'&key=AIzaSyBCj5rD1yeJmhglY3eapLgqW1GC8WZDoP0')
   }
 
 });
