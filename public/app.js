d297f3b917da836390fe35af032ba5fa03ddb598
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-05-27 16:53:48 +0100

    cleaned up directory

--- public/app.js
+++ public/app.js
@@ -1,15 +1,19 @@
 var moviesAroundMe = angular.module('MoviesAroundMe',['ngResource']);
 
-moviesAroundMe.controller('moviesControl', ['OMDb', function(OMDb) {
+moviesAroundMe.controller('moviesControl', function() {
   self = this;
-  omdbData: Object;
 
-  var self = this;
+  movie1 = {Title:"Big",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km", Cinema: "W149AX"};
+  movie2 = {Title:"Home",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km", Cinema: "E162RU"};
+  movie3 = {Title:"Forrest",IMDb:"8.6",Rotten:"5.6",Distance:"0.2km", Cinema: "NN33NY"};
 
-  self.updateMovieRating = function(){
-    console.log('Movie title: ' + self.movieTitle)
-    OMDb.makeRequest(self.movieTitle).then(function(response) {
-      self.imdbRating = response.data.imdbRating;
-    });
-  };
-}]);
+  self.updateMovies = function() {
+    console.log('hello');
+    self.movies = [movie1, movie2, movie3];
+  }
+
+  self.updateMap = function(cinema) {
+  	$("#map-frame").attr('src', 'https://www.google.com/maps/embed/v1/directions?origin='+self.postcode+'&destination='+cinema+'&key=AIzaSyBCj5rD1yeJmhglY3eapLgqW1GC8WZDoP0')
+  }
+
+});
